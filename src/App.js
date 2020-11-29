166ef39efdcb500e8460c894fecd6f73ff33223f
Mandi <haase1020@gmail.com>
2020-11-29 05:46:42 -0500

    useHooks tutorial

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+import { useForm } from './useForm'
+import './App.css';
+
+function App() {
+const [values, handleChange] = useForm({ email:'', password: ''});
+
+  return (
+    <div>
+      <input name='email' value={values.email} onChange={handleChange} />
+      <input
+      type='password'
+      name='password'
+      value={values.password}
+      onChange={handleChange}
+      />
+
+    </div>
+  );
+}
+
+export default App;
